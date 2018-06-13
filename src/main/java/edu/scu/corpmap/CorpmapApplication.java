fa1b6a2dde3e761034b0a96affb97d05370ee2c4
Vicent_Chen <1336754721@qq.com>
2018-06-13 17:06:33 +0800

    创建项目corpmap

---
+++ src/main/java/edu/scu/corpmap/CorpmapApplication.java
@@ -0,0 +1,12 @@
+package edu.scu.corpmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorpmapApplication.class, args);
+	}
+}
